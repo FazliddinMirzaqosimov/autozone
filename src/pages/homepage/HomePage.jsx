ad79eb6efc97e3660acaa4cf7c3f3c90165219f7
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2022-12-22 20:02:30 +0500

    Added dark mode

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import ShowcaseProducts from "../../components/showcase-products/ShowcaseProducts";
 import Showcase from "../../components/showcase/Showcase";
 
